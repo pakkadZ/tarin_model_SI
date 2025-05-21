8aaf76f824af0d0121f933b6a5e2831f60e42fea
pakkadZ <120437692+pakkadZ@users.noreply.github.com>
2025-05-21 14:47:40 +0700

    Si

--- read.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torchvision.ops import nms
-
-boxes = torch.tensor([[0, 0, 10, 10], [0, 0, 9, 9]], dtype=torch.float32).cuda()
-scores = torch.tensor([0.9, 0.8], dtype=torch.float32).cuda()
-
-print(nms(boxes, scores, 0.5))
